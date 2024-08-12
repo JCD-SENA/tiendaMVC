653fbaafb2d0d5a52a26fa212d955a620628348d
SENA <SENA@ARMSCCCCTGOD048.FormacionSena.RED>
2024-08-12 08:33:11 -0500

    Primer commit

---
+++ src/main/java/com/tiendaMVC/tiendaMVC/TiendaMvcApplication.java
@@ -0,0 +1,13 @@
+package com.tiendaMVC.tiendaMVC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaMvcApplication.class, args);
+	}
+
+}
